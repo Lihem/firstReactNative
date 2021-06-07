33e251571523ce393690e9007db6e339c431fe11
Lihem <mlhcsgn10@gmail.com>
2021-06-07 14:53:53 +0300

    using reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,45 +1,58 @@
-import React ,{useState} from 'react'
+import React ,{useReducer} from 'react'
 import {View, Text, StyleSheet } from 'react-native'
 import ColorCounter from "../components/ColorCounter"
 
 const COLOR_INCREMENT = 10
+                         //action denen sey statei nasil degistirecegimizi soyleyen obje
+const reducer = (state, action) =>{     //reducer herzaman bir state objesi donmeli ya guncellenmis bir halini yada kendisini
+    switch(action.type){
+        case 'change_red':
+            if(state.red + action.payload > 255 || state.red + action.payload < 0){
+                return state
+            }
+            return {...state, red: state.red + action.payload}
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0
+                ? state
+                : {...state, green: state.green + action.payload}
+        case 'change_blue':
+            if(state.blue + action.payload > 255 || state.blue + action.payload < 0){
+                return state
+            }
+            return {...state, blue: state.blue + action.payload}
+        default:
+            return state
+    }
+}
+
+    //action icin not:
+    // {colorToChange: 'red' , amount:15}  yerine  {type: 'change_red' , payload: 15} kullanimi yaygin kullanim
+    // type => String that describes the exact change operation we want to make
+    // payload => some data that is critical to the change operation
 
 const SquareScreen = () => {
-    const [red,setRed] = useState(0)
-    const [green,setGreen] = useState(0)
-    const [blue,setBlue] = useState(0)
-
-    const setColor = (myColor,change) => {
-        switch(myColor){
-            case 'red':
-                red + change > 255 || red + change < 0 ? null : setRed(red + change)
-            return
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-            return
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-            return 
-        }
-    }
+    //dispatch <> runMyReducer                   state'in initial degeri
+    const [state,dispatch] = useReducer(reducer,{red: 0, green:0 ,blue:0})
+    const {red,green,blue} = state         //bu sekilde statein icinden degerleri cekebiliyoruz
+    //const [red, green, blue] = [state.red,state.green, state.blue]
 
     return <View>
         <ColorCounter
-            myNum = {red}
-            onIncrease = {() => setColor('red', COLOR_INCREMENT)}
-            onDecrease = {() => setColor('red' , -1 * COLOR_INCREMENT)}
+            myNum = {red}              //dispatch arguman olarak action aliyor burda action objeleri olusturuyoruz
+            onIncrease = {() => dispatch({type: 'change_red' , payload: COLOR_INCREMENT})}
+            onDecrease = {() => dispatch({type: 'change_red' , payload: -COLOR_INCREMENT})}
             myTitle = "Red"
             />
         <ColorCounter 
             myNum = {green}
-            onDecrease = {() => setColor('green' ,- COLOR_INCREMENT)}
-            onIncrease = {() => setColor('green' , COLOR_INCREMENT)}
+            onDecrease = {() => dispatch({type: 'change_green' , payload: -COLOR_INCREMENT})}
+            onIncrease = {() => dispatch({type: 'change_green' , payload: COLOR_INCREMENT})}
             myTitle = "Green"
             />
         <ColorCounter 
             myNum = {blue}
-            onDecrease = {() => setColor('blue' ,- COLOR_INCREMENT)}
-            onIncrease = {() => setColor('blue' , COLOR_INCREMENT)}       
+            onDecrease = {() => dispatch({type: 'change_blue' , payload: -COLOR_INCREMENT})}
+            onIncrease = {() => dispatch({type: 'change_blue' , payload: COLOR_INCREMENT})}       
             myTitle = "Blue"
             />
         <View style ={{ height: 150 , width: 150 , backgroundColor : `rgb(${red}, ${green}, ${blue})`}}/>
